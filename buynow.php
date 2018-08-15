2409355223ad8ff6ab954a351017cc9c6a6ca936
zaincashu <34888458+zaincashu@users.noreply.github.com>
2018-08-15 07:55:37 +0300

    Add files via upload

---
+++ buynow.php
@@ -0,0 +1,13 @@
+
+<?
+echo"
+<form action='https://www.2checkout.com/checkout/purchase' method='post'>
+  <input type='hidden' name='sid' value='203831924'>
+  <input type='hidden' name='quantity' value='1'>
+  <input type='hidden' name='product_id' value='1'>
+  <input name='submit' type='submit' value='Buy from 2CO' >
+</form>
+<span>2Checkout.com Inc. (Ohio, USA) is a payment facilitator for goods and services provided by eBooks&eGift-card.</span>
+",
+
+?>
